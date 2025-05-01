29f97dba3f113ab15492ba02c623f5ff0d515e4d
Riyan Dzulfikar <127158603+riyandzulfikar@users.noreply.github.com>
2025-05-01 09:21:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 
 const siswaValid = [
     {
-        nama: "Alifah Najla",
         nisn: "12345",
         pdfUrl: "https://drive.google.com/file/d/1GrzH6wg16CIv3z-KSumWMoMQFJ7BLKqe/view"
     },
     {
-        nama: "Nur Afifah",
         nisn: "12346",
         pdfUrl: "https://drive.google.com/file/d/1nprYvi2gJGrn6V-2qW6pf0FLbm-cDtOB/view"
     }
@@ -16,13 +14,11 @@ const siswaValid = [
 document.getElementById('formKelulusan').addEventListener('submit', function(e) {
     e.preventDefault();
     
-    const nama = document.getElementById('nama').value.trim();
     const nisn = document.getElementById('nisn').value.trim();
     
     // Cari siswa yang sesuai
     const siswa = siswaValid.find(s => 
         s.nisn === nisn && 
-        s.nama.toLowerCase() === nama.toLowerCase()
     );
     
     if (siswa) {
